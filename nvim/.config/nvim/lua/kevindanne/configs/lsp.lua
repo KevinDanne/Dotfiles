305d6966f72e95fe6c32f09ce5b0af4ba055edbf
Kevin Danne <it@kevindanne.com>
2024-04-17 17:09:26 +0200

    fix: use <leader>cf to format code instead of <leader>f

--- nvim/.config/nvim/lua/kevindanne/configs/lsp.lua
+++ nvim/.config/nvim/lua/kevindanne/configs/lsp.lua
@@ -46,7 +46,7 @@ local on_attach = function(client, bufnr)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
     vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set('n', '<space>f', function()
+    vim.keymap.set('n', '<space>cf', function()
       vim.lsp.buf.format { async = true }
     end, opts)
 
